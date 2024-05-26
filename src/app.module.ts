9501908bac19a386b0d3da6e6b03160da3ec875d
bjahn <dksqodwns@gmail.com>
2024-05-26 22:57:32 +0900

    nestjs에서 postgresql을 쓰려면 설정이 다르다!

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,53 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberController } from './member/member.controller';
 import { MemberService } from './member/member.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  Comment,
+  Group,
+  Matching,
+  MatchingGroup,
+  Message,
+  Post,
+  Profile,
+  School,
+  User,
+} from './entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      entities: [
+        User,
+        School,
+        Profile,
+        Post,
+        Message,
+        Matching,
+        MatchingGroup,
+        Group,
+        Comment,
+      ],
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([
+      User,
+      School,
+      Profile,
+      Post,
+      Message,
+      Matching,
+      MatchingGroup,
+      Group,
+      Comment,
+    ]),
+  ],
   controllers: [AppController, MemberController],
   providers: [AppService, MemberService],
 })
